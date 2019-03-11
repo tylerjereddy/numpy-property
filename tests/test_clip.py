a488a2cfa45ab3ecb1e85878aa16c936f572f732
Tyler Reddy <tyler.je.reddy@gmail.com>
2019-03-10 19:22:20 -0700

    Add new clip() test involving arrays of floats for clip limits

--- tests/test_clip.py
+++ tests/test_clip.py
@@ -22,3 +22,16 @@ def test_clip_scalar_ints(arr, amin, amax):
     # here because we can have object type in output
     # which causes issues
     assert result.tolist() == expected.tolist()
+
+# next, move to the case of floats & arrays
+# for the clip limits
+@given(hynp.arrays(np.float64, 10),
+       hynp.arrays(np.float64, 10),
+       hynp.arrays(np.float64, 10),
+       )
+def test_clip_array_floats(arr, amin, amax):
+    result = np.clip(arr, amin, amax)
+    # preserve shape on clip
+    assert result.shape == arr.shape
+    expected = np.maximum(amin, np.minimum(arr, amax))
+    assert_equal(result, expected)
